230410e1ceb6b0e8a817e11c9d279a15dc2b3cda
a0z9 <a0z9@rambler.ru>
2018-11-09 08:03:07 +0300

    1

---
+++ src/main/java/org/andestech/learning/rfb18/g2/AppXmlLib.java
@@ -0,0 +1,43 @@
+package org.andestech.learning.rfb18.g2;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class AppXmlLib {
+
+    public static void main(String[] args)
+    {
+
+    App.feedLibrary();
+
+    Library library = new Library(App.library);
+
+
+
+    try(FileOutputStream fos =
+            new FileOutputStream("e:\\datas\\library.xml"))
+    {
+        JAXBContext context =
+                JAXBContext.newInstance(Library.class);
+
+
+        Marshaller marshaller =
+                context.createMarshaller();
+
+        marshaller.setProperty("jaxb.formatted.output", true);
+
+        marshaller.marshal(library, fos);
+
+
+    }
+    catch (JAXBException | IOException ex){ex.printStackTrace();}
+
+
+
+
+    }
+
+}
